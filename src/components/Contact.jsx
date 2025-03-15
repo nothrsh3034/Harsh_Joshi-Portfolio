86c5fda2d83a1b3b556a3d3f0d5176ec04eb4d76
Harsh Joshi <hrsh3034@gmail.com>
2025-03-15 17:44:02 +0530

    Initial commit

---
+++ src/components/Contact.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { FaInstagram } from "react-icons/fa";
+import { CiFacebook } from "react-icons/ci";
+import { CiLinkedin } from "react-icons/ci";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaGithubSquare } from "react-icons/fa";
+import { SiGmail } from "react-icons/si";
+
+const Contact = () => {
+  return (
+    <>
+      <div className="container contact" id="contact">
+        <h1>CONTACT ME</h1>
+        <div
+          className="contact-icon"
+          data-aos="zoom-in-up"
+          data-aos-duration="1000"
+        >
+          <a href="https://www.instagram.com/harshjoshi_30/" target="_blank" className="items">
+            <FaInstagram className="icons" />
+          </a>
+         
+          <a href="https://www.linkedin.com/in/harsh-joshi-51776932a/" target="_blank" className="items">
+            <CiLinkedin className="icons" />
+          </a>
+          <a href="https://www.google.com" target="_blank" className="items">
+            <FaSquareXTwitter className="icons" />
+          </a>
+          
+          <a
+            href="mailto:hrsh3034@gmail.com"
+            target="_blank"
+            className="items"
+          >
+            <SiGmail className="icons" />
+          </a>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Contact;
